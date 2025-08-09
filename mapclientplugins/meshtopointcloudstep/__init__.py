f9361a7258b8cd51a2b396518ce87b8bc7427c90
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-08-09 13:35:55 +1200

    Remove .git from end of step location definition.

--- mapclientplugins/meshtopointcloudstep/__init__.py
+++ mapclientplugins/meshtopointcloudstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin - Generated from MAP Client v0.24.0
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'MeshToPointCloud'
-__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.meshtopointcloudstep.git'
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.meshtopointcloudstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.meshtopointcloudstep import step

6c3dcf383948864f9cbb7a76e6db08d61cc4f383
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-07-30 14:00:38 +1200

    Set version to 0.2.0.

--- mapclientplugins/meshtopointcloudstep/__init__.py
+++ mapclientplugins/meshtopointcloudstep/__init__.py
@@ -3,7 +3,7 @@
 MAP Client Plugin - Generated from MAP Client v0.24.0
 """
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'MeshToPointCloud'
 __location__ = 'https://github.com/mapclient-plugins/mapclientplugins.meshtopointcloudstep'
